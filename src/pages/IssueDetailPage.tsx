4b585a55e7ecd5cb69c84590ffc0272e7163063f
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-08-30 23:07:35 +0900

    FEAT: react-markdown, remark-gfm로 마크다운 문자열 변환하여 렌더링

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -4,6 +4,8 @@ import { ORGANIZATION_NAME, REPOSITORY_NAME } from '../constants';
 import { useIssueContext } from '../context/issueContext';
 import { useParams } from 'react-router-dom';
 import IssueItem from '../components/IssueItem';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default function IssueDetailPage() {
   const { state } = useIssueContext();
@@ -18,7 +20,14 @@ export default function IssueDetailPage() {
         <article>
           <img src={issueData.user.avatar_url} alt='' />
           <IssueItem issueData={issueData} />
-          <p>{issueData.body}</p>
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            components={{
+              img: ({ ...props }) => <img style={{ maxWidth: '100%' }} {...props} alt='' />,
+            }}
+          >
+            {issueData.body}
+          </ReactMarkdown>
         </article>
       )}
     </>
