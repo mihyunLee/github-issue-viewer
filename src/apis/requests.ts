7fe509072b74d7576ff047c1b1e245dad5e4c0c3
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-08-30 23:27:05 +0900

    FEAT: 코멘트가 많은 순으로 이슈 리스트 API 요청

--- src/apis/requests.ts
+++ src/apis/requests.ts
@@ -1,6 +1,10 @@
 import { instance } from './axiosInstance';
 
 export const getIssueList = async () => {
-  const res = await instance.get('/issues');
+  const res = await instance.get('/issues', {
+    params: {
+      sort: 'comments',
+    },
+  });
   return res.data;
 };
